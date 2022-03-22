d862a799b78b2b16787f70773ea38b838c500c4f
D3ATHSCYTH3R <94089013+D3ATHSCYTH3R@users.noreply.github.com>
2022-03-22 18:08:41 -0500

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,4 @@
+9=localStorage.getItem("i1").value;
+8=localStorage.getItem("i2").value;
+pn1=9
+pn2=8
---
+++ main.js
@@ -0,0 +1,5 @@
+if (b1=onclick){
+    window.screen("i2.html");
+    localStorage.setItem("i1", i1);
+    localStorage.setItem("i2", i2);
+}
